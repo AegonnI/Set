ba89e58263e90eef5116fee1a809815543cc1bb6
AegonnI <darthsokol.750@gmail.com>
2024-08-16 15:48:50 +0500

    update1

--- Set/Assets/Scripts/Card.cs
+++ Set/Assets/Scripts/Card.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
 
-    public int type; // 1 - cube, 2 - circle, 3 - romb
-    public int color; // 1 - red, 2 - green, 3 - blue
-    public int count;
-    public int filling; // 1 - empty, 2 - hatch, 3 - fill
+    public int type; // 0 - cube, 1 - circle, 2 - romb
+    public int color; // 0 - red, 1 - green, 2 - blue
+    public int count; // 0 - 1, 1 - 2, 2 - 3
+    public int filling; // 0 - empty, 1 - hatch, 2 - fill
+
+    public Card()
+    {
+        type = -1;
+        color = -1;
+        count = -1;
+        filling = -1;
+    }
 
     // Start is called before the first frame update
     void Start()
--- Set/Assets/Scripts/Spawn.cs
+++ Set/Assets/Scripts/Spawn.cs
@@ -9,10 +9,32 @@ public class Spawn : MonoBehaviour
     public Vector3 spawnPosition = new Vector3(0, 0, 0);
     public Quaternion spawnRotation = Quaternion.identity;
 
+    private bool[,,,] cardBoard = new bool[3,3,3,3];
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(card, spawnPosition, spawnRotation);
+        System.Random rand = new System.Random();
+        for (int i = 0; i < 12; i++)
+        {
+            int xrand = rand.Next(3);
+            int yrand = rand.Next(3);
+            int zrand = rand.Next(3);
+            int wrand = rand.Next(3);
+            if (cardBoard[xrand, yrand, zrand, wrand] == true)
+            {
+
+            }
+            cardBoard[xrand, yrand, zrand, wrand] = true;
+            spawnPosition = new Vector3(i % 3, i / 3, 0);
+            Card crd = card.GetComponent<Card>();
+            crd.type = xrand;
+            crd.color = yrand;
+            crd.count = zrand;
+            crd.filling = wrand;
+            Instantiate(card, spawnPosition, spawnRotation);
+        }
+        //Instantiate(card, spawnPosition, spawnRotation);
     }
 
     // Update is called once per frame
